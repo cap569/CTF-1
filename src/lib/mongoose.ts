7444f38bdbf9b1f76139e341031d803a22f85cef
Yuri <49888082+YuriRDev@users.noreply.github.com>
2025-04-20 11:29:28 -0300

    Fixed multiple connections on mongoose

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
-// @TODO: This makes multiple connections every request
-// we should fix this to avoid redundancy.
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.DATABASE_URL as string);
   } catch (err) {
