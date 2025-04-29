54f157283d4ed4b9552bf451b780209320a9d74f
Yuri <49888082+YuriRDev@users.noreply.github.com>
2025-04-29 10:31:11 -0300

    HotFix for recording

--- src/app/api/review/route.ts
+++ src/app/api/review/route.ts
@@ -67,7 +67,7 @@ export async function POST(req: Request) {
     await connectDB();
     const purchase = await Purchase.findById(bodyParser.data._id);
     purchase.stars = bodyParser.data.stars;
-    purchase.review = 5;
+    purchase.review = bodyParser.data.review;
     if (bodyParser.data.price) purchase.price = bodyParser.data.price;
 
     await purchase.save();
